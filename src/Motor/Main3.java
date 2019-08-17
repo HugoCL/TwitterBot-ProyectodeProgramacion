46b7914b62f129352f7336ad300fac24e30e9083
nicolas Castillo <ncastillo18@alumnos.utalca.cl>
2019-08-17 00:32:48 -0400

    Retweets Timeline
    Primeros retweets hechos sin capacidad de elegir y sin metodo personal.

--- src/Motor/Main3.java
+++ src/Motor/Main3.java
@@ -9,21 +9,7 @@ public class Main3 {
         boolean exito = false;
         TwitterBot bot = new TwitterBot();
         bot.inicializarBot();
-        TwitterBot.Messages mensajes = bot.new Messages();
-        Scanner entrada = new Scanner(System.in);
-        while(!exito){
-            System.out.println("Ingrese el Tweet a enviar:");
-            String tweet = entrada.nextLine();
-            if (tweet.length() > 280){
-                System.out.println("Te pasaste del limite de caracteres :(");
-            }
-            else{
-                System.out.println("Enviando...");
-                mensajes.PublicarTweet(tweet);
-                System.out.println("Se Tweeteo: "+tweet+". Enviado correctamente.");
-                exito = true;
-            }
-        }
+
 
     }
 }
--- src/Motor/TwitterBot.java
+++ src/Motor/TwitterBot.java
@@ -1,20 +1,63 @@
 package Motor;
 import twitter4j.*;
+import twitter4j.auth.AccessToken;
 import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.ArrayList;
 
 public class TwitterBot {
 
     private Twitter twitter;
 
+    private String consumerKey = "y3rodATEKk9OopeZb3bJ49k7L",
+            consumerSecret = "eCkLQgglSpvdD7nUiU6hoH2hoWYEWASAAMRWkfuTyqnhUxLfr0",
+            twitterAccessToken = "1160958881268473856-azR9gn8ajjf1EqlURcy6xjo4LxmjkJ",
+            twitterAccessTokenSecret = "UemukyzySFXAi9jBK9t3TO91tYZfT7cVsxSsPFGnu1i3n",
+            Tweet_ID = "1161699831909429248";
+
     public void inicializarBot() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true);
-        cb.setOAuthConsumerKey("y3rodATEKk9OopeZb3bJ49k7L");
-        cb.setOAuthConsumerSecret("eCkLQgglSpvdD7nUiU6hoH2hoWYEWASAAMRWkfuTyqnhUxLfr0");
-        cb.setOAuthAccessToken("1160958881268473856-azR9gn8ajjf1EqlURcy6xjo4LxmjkJ");
-        cb.setOAuthAccessTokenSecret("UemukyzySFXAi9jBK9t3TO91tYZfT7cVsxSsPFGnu1i3n");
+        cb.setOAuthConsumerKey(consumerKey);
+        cb.setOAuthConsumerSecret(consumerSecret);
+
         TwitterFactory tf = new TwitterFactory(cb.build());
         twitter = tf.getInstance();
+
+        AccessToken acceso = new AccessToken(twitterAccessToken, twitterAccessTokenSecret);
+
+        twitter.setOAuthAccessToken(acceso);
+
+        ArrayList<Status> statuses = new ArrayList<>();
+        int pageno = 1;
+
+        while(true) {
+            try {
+                System.out.println("getting tweets");
+                int size = statuses.size(); // actual tweets count we got
+                Paging page = new Paging(pageno, 200);
+                statuses.addAll(twitter.getUserTimeline("Ramos Overflow - BOT", page));
+                System.out.println("total got : " + statuses.size());
+
+                if (statuses.size() == size) {
+                    break;
+                } // we did not get new tweets so we have done the job
+                pageno++;
+
+            } catch (
+                    TwitterException e) {
+                System.err.print("Failed to search tweets: " + e.getMessage());
+            }
+        }
+
+        try {
+            for (Status t: statuses) {
+                System.out.println(t.getUser().getName() + ": " + t.getText()+ " " + t.getId());
+                twitter.retweetStatus(t.getId());
+            }
+        } catch (TwitterException e) {
+            System.err.print("Failed to search tweets: " + e.getMessage());
+        }
     }
 
     class Messages{
