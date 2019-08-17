06b8112b311813b298d3c79c88430cad6157d68e
Ariel <arxiel29@gmail.com>
2019-08-16 21:48:45 -0400

    Implementación requisitos de Twitter
    Se añadió al controlador de la interfaz el método para validar el ingreso de la cantidad de caracteres determinada por Twitter. Habilitación y deshabilitación de botón para Twittear según texto y validaciones.

--- src/Interfaz/Controller.java
+++ src/Interfaz/Controller.java
@@ -1,4 +1,12 @@
 package Interfaz;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -9,6 +17,10 @@ public class Controller {
 
     private Twitter twitter;
 
+    @FXML private Button publicar_tweetBT;
+    @FXML private TextArea tweet_TA;
+    @FXML private Label caracteres_LB;
+
     public void initialize(){
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true);
@@ -18,10 +30,35 @@ public class Controller {
         cb.setOAuthAccessTokenSecret("V1u62wAEOucjDYGIM3YxWWRTFyjEJSMo3vZNbO0kNjQHN");
         TwitterFactory tf = new TwitterFactory(cb.build());
         twitter = tf.getInstance();
-    }
 
-    public String publicarTwitter(String tweet) throws TwitterException {
-        Status status = twitter.updateStatus("Hola Mundo!");
+        //Inicio de Ventana
+        publicar_tweetBT.setDisable(true);
+
+        //Caracteres
+        KeyFrame frame = new KeyFrame(Duration.millis(100), e -> Caracteres());
+        Timeline timeline = new Timeline(frame);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+    private void Caracteres() {
+        if (!tweet_TA.getText().isEmpty()) {
+            if (tweet_TA.getText().length() >= 280){
+                caracteres_LB.setTextFill(Color.web("#ff0000"));
+                publicar_tweetBT.setDisable(true);
+            }
+            else {
+                caracteres_LB.setTextFill(Color.web("#000000"));
+                publicar_tweetBT.setDisable(false);
+            }
+            caracteres_LB.setText(tweet_TA.getText().length()+"/280");
+        }
+        else    caracteres_LB.setText("0/280");
+
+    }
+    @FXML public String publicarTwitter() throws TwitterException {
+        String tweet = tweet_TA.getText();
+        Status status = twitter.updateStatus(tweet);
+        System.out.println(tweet);
         return status.getText();
     }
 }
--- src/Interfaz/Main.java
+++ src/Interfaz/Main.java
@@ -7,12 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("interfaz.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
