9b19127d8d0e702e6d2a3778c9a7455b53ba244f
Ariel <arxiel29@gmail.com>
2019-08-22 10:30:23 -0400

    Creación ventana follow.
    Se crea ventana de follow con el esqueleto (falta implementar métodos).

---
+++ src/Interfaz/FollowController.java
@@ -0,0 +1,26 @@
+package Interfaz;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class FollowController {
+
+    @FXML private JFXButton followBT;
+    @FXML private JFXButton closeBT;
+    @FXML private JFXTextField nicknameTF;
+
+    @FXML private AnchorPane followAP;
+
+    public void initialize(){
+        followBT.setDisable(true);
+    }
+
+    public void seguir(){
+        System.out.println(nicknameTF.getText());
+    }
+    public void cerrarVentana(){
+        
+    }
+}
