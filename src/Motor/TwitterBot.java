a32db2962b14e70a470ffd2629f65ba5d756e5a1
Hugo <hugointe@gmail.com>
2019-09-10 10:43:01 -0300

    Prototipo de Backup de Tweets
    Se creó el prototipo que permite mantener el respaldo de los últimos Tweets del Timeline

--- src/Motor/TwitterBot.java
+++ src/Motor/TwitterBot.java
@@ -185,6 +185,8 @@ public class TwitterBot implements Serializable {
      * Segunda clase interna que se encarga de las funciones relacionadas a contenidos externos
      */
     public class Feed {
+
+        private ArrayList<Tweet> backupTweets;
         private ArrayList<Tweet> tweets = new ArrayList<>();
         /***
          * Permite la obtención de los tweets del timeline de la cuenta ingresada
@@ -208,9 +210,13 @@ public class TwitterBot implements Serializable {
                     if (tweets.size() != 0){
                         return null;
                     }
+                    if(e.getErrorCode()== 429){
+                    }
+                    backupTweets = tweets;
                     return tweets;
                 }
             }
+            backupTweets = tweets;
             return tweets;
         }
 
