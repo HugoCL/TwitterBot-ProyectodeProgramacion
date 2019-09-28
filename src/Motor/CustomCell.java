419f09e267da13609de466b24795bfb00857e975
Nicolas Castillo <ncastillo18@alumnos.utalca.cl>
2019-09-28 18:27:51 -0300

    Cambio y agregado de iconos
    Se agregan like y retweet en iconos

--- src/Motor/CustomCell.java
+++ src/Motor/CustomCell.java
@@ -13,7 +13,8 @@ import javafx.scene.layout.HBox;
 import twitter4j.Twitter;
 
 public class CustomCell extends ListCell<Tweet> {
-    private JFXButton actionBtn;
+    private JFXButton like_BT;
+    private JFXButton retweet_BT;
     private Label name;
     private Label numero;
     private TextArea mensaje;
@@ -33,26 +34,29 @@ public class CustomCell extends ListCell<Tweet> {
             }
         });
 
-        actionBtn = new JFXButton();
-        actionBtn.setMinSize(15, 15);
-        actionBtn.setMaxSize(15, 15);
-        actionBtn.getStyleClass().add("GrayHeart-buttton");
-        actionBtn.setOnAction(new EventHandler<ActionEvent>() {
+        like_BT = new JFXButton();
+        like_BT.setGraphic(new ImageView(new Image("Imagenes/heart.png", 20,20,false,true)));
+        like_BT.getStyleClass().add("GrayHeart-buttton");
+        like_BT.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Action: "+getItem().getMensaje());
-                System.out.println(actionBtn.getStyleClass().get(2));
+                System.out.println(like_BT.getStyleClass().get(2));
                 if (!tf){
-                    actionBtn.getStyleClass().set(2, "RedHeart-buttton");System.out.println(tf);
+                    like_BT.getStyleClass().set(2, "RedHeart-buttton");System.out.println(tf);
                     tf = true;
 
                 }else{
-                    actionBtn.getStyleClass().set(2, "GrayHeart-buttton");System.out.println(tf);
+                    like_BT.getStyleClass().set(2, "GrayHeart-buttton");System.out.println(tf);
                     tf = false;
 
                 }
             }
         });
+
+        retweet_BT = new JFXButton();
+        retweet_BT.setGraphic(new ImageView(new Image("Imagenes/retweet.png", 20,20,false, true)));
+
         name = new Label();
         name.getStyleClass().add("label");
 
@@ -74,8 +78,8 @@ public class CustomCell extends ListCell<Tweet> {
         pane.add(imagen, 0, 0);
         pane.add(name, 1, 0);
         pane.add(mensaje, 0, 1, 2, 1);
-        pane.add(new HBox(1, new JFXButton("algo"), numero), 0, 2);
-        pane.add(new HBox(1, actionBtn, numero), 1, 2);
+        pane.add(new HBox(1, retweet_BT, numero), 0, 2);
+        pane.add(new HBox(1, like_BT, numero), 1, 2);
         setText(null);
     }
 
