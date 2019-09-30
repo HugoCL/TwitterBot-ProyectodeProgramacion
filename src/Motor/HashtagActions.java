ee592bde94721662f76c4678b4631f9b8e5e652d
Hugo <hugointe@gmail.com>
2019-09-30 20:46:40 -0300

    Se creó el método preliminar para responder a Hashtags
    Se creó el método encargado de responder a los # de los usuarios y realizar una acción consecuente. También se hizo un cambio en el nombre de la clase

---
+++ src/Motor/HashtagActions.java
@@ -0,0 +1,35 @@
+package Motor;
+
+import twitter4j.*;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class HashtagActions {
+
+    private static Twitter twitter = TwitterBot.getInstance().getBOT().getTwitter();
+
+
+    private static void HashTagActions(ArrayList<Tweet> tweets) {
+        Usuario user = new Usuario();
+        Feed feed = new Feed();
+        try {
+            for (Tweet tweet: tweets)
+                for (HashtagEntity hashtagEntity : twitter.showStatus(tweet.getId()).getHashtagEntities())
+                    if(hashtagEntity.getText().equalsIgnoreCase("seguir")){
+                        Status statusFollow = twitter.showStatus(tweet.getId());
+                        user.follow(statusFollow.getUser().getScreenName());
+                    }
+                    else if (hashtagEntity.getText().equalsIgnoreCase("darlike")){
+                        Status statusLike = twitter.showStatus(tweet.getId());
+                        feed.like(statusLike.getId());
+                    }
+                    else if (hashtagEntity.getText().equalsIgnoreCase("retwittear")){
+                        Status statusRetweet = twitter.showStatus(tweet.getId());
+                        feed.retweet(statusRetweet.getId());
+                    }
+        } catch (TwitterException e) {
+            System.out.println(e.getErrorMessage());
+        }
+    }
+}
