070965bc376703e06df961c10b5e663cbf3ef749
Hugo <hugointe@gmail.com>
2019-10-05 20:59:04 -0300

    Se actualizó el #Seguir
    Ahora el método usa una implementación independiente del follow. Tambien ahora responde al usuario que solicitó la acción, notificándole si se realizó o no la acción correctamente.

--- src/Motor/HashtagActions.java
+++ src/Motor/HashtagActions.java
@@ -1,9 +1,6 @@
 package Motor;
 
-import twitter4j.HashtagEntity;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
+import twitter4j.*;
 
 import java.util.ArrayList;
 
@@ -15,12 +12,51 @@ public class HashtagActions {
     public void HashTagActions(ArrayList<Tweet> tweets) {
         Usuario user = new Usuario();
         Feed feed = new Feed();
+        Messages messages = new Messages();
         try {
             for (Tweet tweet: tweets)
                 for (HashtagEntity hashtagEntity : twitter.showStatus(tweet.getId()).getHashtagEntities())
                     if(hashtagEntity.getText().equalsIgnoreCase("seguir")){
                         Status statusFollow = twitter.showStatus(tweet.getId());
-                        user.follow(statusFollow.getUser().getScreenName());
+                        String name = statusFollow.getUser().getScreenName();
+                        try {
+                            if (!twitter.showFriendship(twitter.getScreenName(), name).isSourceFollowingTarget() && !twitter.getScreenName().equals(name)){
+                                twitter.createFriendship(name);
+                                if (!twitter.showFriendship(twitter.getScreenName(), name).isSourceFollowingTarget()){
+                                    StatusUpdate statusUpdate = new StatusUpdate("Espere respuesta de @"+name);
+                                    statusUpdate.setInReplyToStatusId(statusFollow.getId());
+                                    twitter.updateStatus(statusUpdate);
+                                }
+                                StatusUpdate statusUpdate = new StatusUpdate("Se sigue correctamente a @"+name);
+                                statusUpdate.setInReplyToStatusId(statusFollow.getId());
+                                twitter.updateStatus(statusUpdate);
+                            }
+                            else if(twitter.getScreenName().equals(name)){
+                                StatusUpdate statusUpdate = new StatusUpdate("ERROR: No se pudo realizar la acción por # (No puedes seguirte a ti mismo)");
+                                statusUpdate.setInReplyToStatusId(statusFollow.getId());
+                                twitter.updateStatus(statusUpdate);
+                            }
+                            else{
+                                StatusUpdate statusUpdate = new StatusUpdate("Ya se siguia al usuario: @"+name.);
+                                statusUpdate.setInReplyToStatusId(statusFollow.getId());
+                                twitter.updateStatus(statusUpdate);
+
+                            }
+
+                        } catch (TwitterException e) {
+                            try {
+                                if (!twitter.showFriendship(twitter.getScreenName(), name).isSourceFollowingTarget()){
+                                    StatusUpdate statusUpdate = new StatusUpdate("Espere respuesta de @"+name);
+                                    statusUpdate.setInReplyToStatusId(statusFollow.getId());
+                                    twitter.updateStatus(statusUpdate);
+                                }
+                                    
+                            } catch (TwitterException ex) {
+                                StatusUpdate statusUpdate = new StatusUpdate("No se encuentra al usuario: @"+name);
+                                statusUpdate.setInReplyToStatusId(statusFollow.getId());
+                                twitter.updateStatus(statusUpdate);
+                            }
+                        }
                     }
                     else if (hashtagEntity.getText().equalsIgnoreCase("darlike")){
                         Status statusLike = twitter.showStatus(tweet.getId());
