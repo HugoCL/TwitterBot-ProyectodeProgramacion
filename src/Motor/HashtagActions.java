e2c73273f32bdbc3626909927a3634d70973df16
Hugo <hugointe@gmail.com>
2019-10-06 23:48:11 -0300

    Se actualizó el #Retwittear
    Se independizó la parte del método que se encarga de los # encargada de los retweets. Tambien se agregaron las respuestas para notificar de las acciones.

--- src/Motor/HashtagActions.java
+++ src/Motor/HashtagActions.java
@@ -21,33 +21,33 @@ public class HashtagActions {
                             if (!twitter.showFriendship(twitter.getScreenName(), name).isSourceFollowingTarget() && !twitter.getScreenName().equals(name)){
                                 twitter.createFriendship(name);
                                 if (!twitter.showFriendship(twitter.getScreenName(), name).isSourceFollowingTarget()){
-                                    StatusUpdate statusUpdate = new StatusUpdate("Acción automatizada -> Espere respuesta de @"+name);
+                                    StatusUpdate statusUpdate = new StatusUpdate("Espere respuesta de @"+name);
                                     statusUpdate.setInReplyToStatusId(statusFollow.getId());
                                     twitter.updateStatus(statusUpdate);
                                 }
-                                StatusUpdate statusUpdate = new StatusUpdate("Acción automatizada -> Se sigue correctamente a @"+name);
+                                StatusUpdate statusUpdate = new StatusUpdate("Se está siguiendo correctamente a @"+name);
                                 statusUpdate.setInReplyToStatusId(statusFollow.getId());
                                 twitter.updateStatus(statusUpdate);
                             }
                             else if(twitter.getScreenName().equals(name)){
-                                StatusUpdate statusUpdate = new StatusUpdate("Acción automatizada -> ERROR: No se pudo realizar la acción por # (No puedes seguirte a ti mismo)");
+                                StatusUpdate statusUpdate = new StatusUpdate("ERROR: No se pudo realizar la acción por # (No puedes seguirte a ti mismo)");
                                 statusUpdate.setInReplyToStatusId(statusFollow.getId());
                                 twitter.updateStatus(statusUpdate);
                             }
                             else{
-                                StatusUpdate statusUpdate = new StatusUpdate("Acción automatizada -> Ya se siguia a @"+name);
+                                StatusUpdate statusUpdate = new StatusUpdate("¡Vaya! Ya se siguia a @"+name);
                                 statusUpdate.setInReplyToStatusId(statusFollow.getId());
                                 twitter.updateStatus(statusUpdate);
                             }
                         } catch (TwitterException e) {
                             try {
                                 if (!twitter.showFriendship(twitter.getScreenName(), name).isSourceFollowingTarget()){
-                                    StatusUpdate statusUpdate = new StatusUpdate("Acción automatizada -> Espere respuesta de @"+name);
+                                    StatusUpdate statusUpdate = new StatusUpdate("Espere respuesta de @"+name);
                                     statusUpdate.setInReplyToStatusId(statusFollow.getId());
                                     twitter.updateStatus(statusUpdate);
                                 }
                             } catch (TwitterException ex) {
-                                StatusUpdate statusUpdate = new StatusUpdate("Acción automatizada -> No se encuentra a @"+name);
+                                StatusUpdate statusUpdate = new StatusUpdate("Ups :( No se encuentra a @"+name);
                                 statusUpdate.setInReplyToStatusId(statusFollow.getId());
                                 twitter.updateStatus(statusUpdate);
                             }
@@ -75,7 +75,23 @@ public class HashtagActions {
                     }
                     else if (hashtagEntity.getText().equalsIgnoreCase("retwittear")){
                         Status statusRetweet = twitter.showStatus(tweet.getId());
-                        feed.retweet(statusRetweet.getId());
+                        try {
+                            if (!twitter.showStatus(statusRetweet.getId()).isRetweetedByMe()) {
+                                twitter.retweetStatus(statusRetweet.getId());
+                                StatusUpdate statusUpdate = new StatusUpdate("Se ha retwitteado exitosamente @"+statusRetweet.getUser().getScreenName());
+                                statusUpdate.setInReplyToStatusId(statusRetweet.getId());
+                                twitter.updateStatus(statusUpdate);
+                            } else{
+                                twitter.unRetweetStatus(statusRetweet.getId());
+                                StatusUpdate statusUpdate = new StatusUpdate("Se ha quitado el retweet exitosamente @"+statusRetweet.getUser().getScreenName());
+                                statusUpdate.setInReplyToStatusId(statusRetweet.getId());
+                                twitter.updateStatus(statusUpdate);
+                            }
+                        } catch (TwitterException e) {
+                            StatusUpdate statusUpdate = new StatusUpdate("Vaya, ocurrió un error al momento de retwittear :( @"+statusRetweet.getUser().getScreenName());
+                            statusUpdate.setInReplyToStatusId(statusRetweet.getId());
+                            twitter.updateStatus(statusUpdate);
+                        }
                     }
         } catch (TwitterException e) {
             System.out.println(e.getErrorMessage());
